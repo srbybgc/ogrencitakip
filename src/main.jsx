e630f9eee0df80111ff8d91d0672dfdad97995f4
srbybgc <srbybgc@gmail.com>
2026-09-10 20:52:07 +0300

    feat: mount archive and recycle bin manager

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import AuthGate from './AuthGate'
 import StudentOverlay from './StudentOverlay'
+import LifecycleOverlay from './LifecycleOverlay'
 import './firebase'
 import './styles.css'
 import { repairStoredData } from './integrity'
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthGate>
       <App />
       <StudentOverlay />
+      <LifecycleOverlay />
     </AuthGate>
   </React.StrictMode>,
 )
