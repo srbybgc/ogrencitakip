b7a5a7906feeee609874fc6abd50e2918bedfb48
srbybgc <srbybgc@gmail.com>
2026-09-11 16:40:46 +0300

    Fix Vite base path for Firebase Hosting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = process.env.DEPLOY_TARGET === 'github-pages' ? '/ogrencitakip/' : '/'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/ogrencitakip/',
+  base,
 })
